f68004c68bc87dab8ca0af5f36858cbcca630d2d
Ronald Claveau <sousmangoosta@aliel.fr>
2025-10-16 14:23:54 +0200

    patch for 6.17

--- drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
+++ drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
@@ -4582,7 +4582,7 @@ static void kbase_mmu_mark_non_movable(struct kbase_device *const kbdev, struct 
 	if (IS_PAGE_MOVABLE(page_md->status))
 		page_md->status = PAGE_MOVABLE_CLEAR(page_md->status);
 
-	__ClearPageMovable(page);
+	//__ClearPageMovable(page);
 	spin_unlock(&page_md->migrate_lock);
 	unlock_page(page);
 }
